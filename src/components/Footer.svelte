4a07281dcbd34fd1d40724338993b16d742da931
Mubasher Mukhtar <mubasherali0331@gmail.com>
2023-03-19 21:41:34 +0500

    changed copyright

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,15 +1,15 @@
-<footer><div class="copyright">Copyright &copy; 2021 Poll Ninja</div></footer>
+<footer><div class="copyright">Copyright &copy; 2023 Poll Ninja</div></footer>
 
 <style>
-  footer {
-    padding: 4rem;
-    text-align: center;
-  }
-  .copyright {
-    color: #aaa;
-    font-size: 1.4rem;
-    display: inline-block;
-    padding: 2rem;
-    border-top: 1px solid #ddd;
-  }
+    footer {
+        padding: 4rem;
+        text-align: center;
+    }
+    .copyright {
+        color: #aaa;
+        font-size: 1.4rem;
+        display: inline-block;
+        padding: 2rem;
+        border-top: 1px solid #ddd;
+    }
 </style>
